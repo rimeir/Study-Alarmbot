a7c25fad3fee802d6fa004b3c7935151f7cc338b
rimeir <skfsk1602@gmail.com>
2024-10-14 10:05:38 +0900

    코드 수정

--- alarmbot.py
+++ alarmbot.py
@@ -19,31 +19,15 @@ scheduler = AsyncIOScheduler()
 BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
 
-# 스터디 알람
-# 월요일 오전 10시에 보내는 메시지
-def ct_mon_mes():
+ct_mes1 = "@everyone \n📅 스터디 마감 D-1! \n이번 주도 화이팅!"
+ct_mes2_am = "@everyone \n📅 스터디 마감 D-0! \n모두 스터디 화이팅!"
+ct_mes2_pm = "@everyone \n📅 스터디 마감 2시간 전! \n잘 제출되었는지 확인하세요!"
+keep_mes = "@everyone \n👍 Keep 작성 알람! \n12시 전까지 keep을 작성해주세요!"
+
+def alarm(mes):
     channel = bot.get_channel(CHANNEL_ID)
     if channel:
-        bot.loop.create_task(channel.send("@everyone \n📅 스터디 마감 D-1! \n이번 주도 화이팅!"))
-
-# 화요일 오전 10시에 보내는 메시지
-def ct_tue_am_mes():
-    channel = bot.get_channel(CHANNEL_ID)
-    if channel:
-        bot.loop.create_task(channel.send("@everyone \n📅 스터디 마감 D-0! \n모두 스터디 화이팅!"))
-
-# 화요일 오후 10시에 보내는 메시지
-def ct_tue_pm_mes():
-    channel = bot.get_channel(CHANNEL_ID)
-    if channel:
-        bot.loop.create_task(channel.send("@everyone \n📅 스터디 마감 2시간 전! \n잘 제출되었는지 확인하세요!"))
-
-# 정처기 알람
-def EIP_mes():
-    channel = bot.get_channel(CHANNEL_ID)
-    if channel:
-        bot.loop.create_task(channel.send("@everyone \n📝 정처기 스터디 마감일! \n디스코드에 인증하는거 잊지말기! 모두 화이팅🫶"))
-
+        bot.loop.create_task(channel.send(mes))
 
 @bot.event
 async def on_ready():
@@ -51,17 +35,16 @@ async def on_ready():
 
     # 스터디 알람
     # 매주 월요일 오전 10시에 메시지 보내기
-    scheduler.add_job(ct_mon_mes, CronTrigger(day_of_week="mon", hour=10, minute=0))
+    scheduler.add_job(alarm(ct_mes1), CronTrigger(day_of_week="mon", hour=10, minute=0))
 
     # 매주 화요일 오전 10시에 메시지 보내기
-    scheduler.add_job(ct_tue_am_mes, CronTrigger(day_of_week="tue", hour=10, minute=0))
+    scheduler.add_job(alarm(ct_mes2_am), CronTrigger(day_of_week="tue", hour=10, minute=0))
 
     # 매주 화요일 오후 10시에 메시지 보내기
-    scheduler.add_job(ct_tue_pm_mes, CronTrigger(day_of_week="tue", hour=22, minute=0))
+    scheduler.add_job(alarm(ct_mes2_pm), CronTrigger(day_of_week="tue", hour=22, minute=0))
 
-    # 정처기 알람
-    # 매주 월, 수, 금요일 오후 6시에 메시지 보내기
-    scheduler.add_job(EIP_mes, CronTrigger(day_of_week="mon,wed,fri", hour=18, minute=0))
+    # 평일 오전 11시에 메시지 보내기
+    scheduler.add_job(alarm(keep_mes), CronTrigger(day_of_week="mon-fri", hour=10, minute=0))
 
     scheduler.start()
 
