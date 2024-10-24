da59d24c142422a69dd41329453a41e25160617e
rimeir <skfsk1602@gmail.com>
2024-10-24 10:37:17 +0900

    오늘 할일 기능 추가

--- alarmbot.py
+++ alarmbot.py
@@ -4,9 +4,13 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 import os
 from dotenv import load_dotenv
+from datetime import datetime
+import asyncio
+import schedule
 
 load_dotenv()
 
+# 인텐스 설정
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
@@ -18,6 +22,10 @@ scheduler = AsyncIOScheduler()
 # .env 파일에서 토큰과 채널 ID 불러오기
 BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
+CHANNEL_ID_TODO = int(os.getenv('DISCORD_CHANNEL_ID_TODO'))
+
+DAYS_OF_WEEK = ['월', '화', '수', '목', '금', '토', '일']
+days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
 # 스터디 알람
 # 월요일 오전 10시에 보내는 메시지
@@ -38,12 +46,27 @@ def ct_tue_pm_mes():
     if channel:
         bot.loop.create_task(channel.send("@everyone \n📅 스터디 마감 2시간 전! \n잘 제출되었는지 확인하세요!"))
 
-# 회고 알람
-def keep_mes():
-    channel = bot.get_channel(CHANNEL_ID)
+# 오늘 할 일 알람
+async def todo_mes():
+    channel = bot.get_channel(CHANNEL_ID_TODO)
+    now = datetime.now()
+    month_day = now.strftime('%m/%d')
+    weekday = DAYS_OF_WEEK[now.weekday()]
     if channel:
-        bot.loop.create_task(channel.send("@everyone \n👍 Keep 작성 마감 1시간 전! \n노션 회고에 오늘 할 일을 작성했는지 확인하세요!"))
+        await channel.send(f"@everyone\n👍 {month_day}({weekday}) 오늘의 할 일! \n해당 메시지 스레드로 오늘 할 일을 작성해주세요!")
 
+# 오늘 할 일 매주 월~토요일까지 오전 10시에 알람 설정
+def todo_alarm():
+    for day in days:
+        getattr(schedule.every(), day).at("10:00").do(
+            lambda: asyncio.run_coroutine_threadsafe(todo_mes(), bot.loop)
+        )
+
+# 스케줄러 백그라운드에서 실행
+async def scheduler_loop():
+    while True:
+        schedule.run_pending()
+        await asyncio.sleep(10)
 
 @bot.event
 async def on_ready():
@@ -57,11 +80,12 @@ async def on_ready():
     scheduler.add_job(ct_tue_am_mes, CronTrigger(day_of_week="tue", hour=10, minute=0))
 
     # 매주 화요일 오후 10시에 메시지 보내기
-    scheduler.add_job(ct_tue_pm_mes, CronTrigger(day_of_week="tue", hour=22, minute=0))
+    scheduler.add_job(ct_tue_pm_mes, CronTrigger(day_of_week="wed", hour=22, minute=0))
 
-    # 회고 알람
-    # 매주 월~금 오전 11시에 메시지 보내기
-    scheduler.add_job(keep_mes, CronTrigger(day_of_week="mon-fri", hour=11, minute=0))
+    # 오늘 할 일 알람 설정
+    todo_alarm()
+    # 오늘 할 일에 날짜를 계속 업데이트하기 위해 루프
+    asyncio.create_task(scheduler_loop())
 
     scheduler.start()
 
